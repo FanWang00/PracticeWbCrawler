bdd083b26a16715bd489965aeb7521d1aff78d98
Unknown <anfangwf@outlook.com>
2017-03-21 03:36:15 +0100

    1

--- physicalweekly.py
+++ physicalweekly.py
@@ -16,16 +16,13 @@ start_link = 'http://physikmethoden.weebly.com'
 content_list1 = []
 
 
-def content_cri(tag):
-    #return re.compile('wsite-menu-subitem').search(class_) and href 
-    pass
     
 def get_link(start_link):
     response = requests.get(start_link)
     soup = BeautifulSoup(response.text,'lxml')
     content_list_temp = soup.find_all('a',class_=['wsite-menu-subitem', 'wsite-menu-item'])
     # print(content_list_temp)
-    
+    content_list = []
     for link in content_list_temp:
         content_list.append(link.get('href'))
         # print(soup)
@@ -50,29 +47,21 @@ def Download_file(start_link, original_link):
             content_list1.append(original_link+link.get('href'))
         #content_list1.append(link)
         # print(soup)
-            print(content_list1)
+            #print(content_list1)
     return content_list1
 
-
-
-def get_pdf_link(link):
-    response_temp = requests.get(link)
-    soup_pdf_temp = BeautifulSoup(response_temp.text,'html')
-    #content_pdf_temp = soup_temp.find_all('a', href = True)
-    #content_pdf_temp = soup_temp
-    #while (content_pdf_temp.get.endswith('.pdf')):
-    for link in soup_pdf_temp.find_all('a'):
-        print(link)
-        print(link.get())
   
-    
 down_pdf_list=[]
 #get_link2(link_new[3],start_link)
 for down_link in link_new:
-    Download_file(down_link, start_link)
+    down_pdf_list = Download_file(down_link, start_link)
     down_pdf_list += down_pdf_list
+    print(down_pdf_list)
 #print(down_pdf_list)
+print(down_pdf_list)
 print('link fetch complete, download start')
+
+'''
 page = 0
 for j in down_pdf_list:
     response = requests.get(j, stream=True)
@@ -85,3 +74,4 @@ for j in down_pdf_list:
         print('ok')
     page += 1
     del response
+'''
